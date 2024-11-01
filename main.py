061d699ae9b8219804025473daa83e9ffc15cc1c
muhammad mujtaba hussain <mujtabasss06@gmail.com>
2024-11-02 00:06:44 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+print("hello world")
+print(True)
+print('someone said : "this" is good ')
+print("someone said : 'this' is good ")
+print('''someone 'said' :    "this" is good ''')
+print('''someone
+      said : 'this' is good ''')
+print("uzair" ,end="  ")
+print("mujtaba")
+print("hello", "mujtaba","qadeer","intezar",sep="-")
+## escape sequence
+print("names are  following : \n 1.mujtaba\t2.ali raza ")
+print("hello \"how\" \'are\' ")# for double and single qoutation showing 
+#print("\U0001F600)
+NAME = 23
+"someone said \n\t\t \t\"ali\" \t is a \'good\' boy \n \t \t but \n he often make mistakes \n\n use variables to store your name and other info and print it in a meangingful way"
+name = "mujtaba"
+age = " 21" 
+print(f"my name is {name} and my age is {age}")
+num = 12 + 3j
+print(type(num))
+string = str(23)
+print(type(string))
+a = 10
+b = 5
+print(a//b)
+print(a%b)
+a =+ 5
+hello = int(input("enter the first number : "))
+print(type(age))
